2d3752efcbcf74cb7ad2b913cf80e69dbb1d4def
Rifqi Oktario <rifqi.oktario@dansmultipro.com>
2023-07-29 11:45:29 +0700

    first commit

---
+++ src/main/java/basicspring/learningconfigproperties/LearningConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package basicspring.learningconfigproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningConfigPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningConfigPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basicspring/learningconfigproperties/LearningConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package basicspring.learningconfigproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningConfigPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
